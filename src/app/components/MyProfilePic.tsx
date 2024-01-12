3c602956637ee9c09147003ae74619022d6caf8e
maryamHabibpourr <Einesheikh_12@yahoo.com>
2024-01-12 21:46:22 +0330

    implement whole project

---
+++ src/app/components/MyProfilePic.tsx
@@ -0,0 +1,16 @@
+import Image from "next/image"
+
+export default function MyProfilePic() {
+    return (
+        <section className="w-full mx-auto">
+            <Image
+                className="border-4 border-slate-800 drop-shadow-xl shadow-black rounded-full mx-auto mt-8"
+                src="/images/maryam.jpg"
+                width={200}
+                height={200}
+                alt="Maryam Habibpour"
+                priority={true}
+            />
+        </section>
+    )
+}
